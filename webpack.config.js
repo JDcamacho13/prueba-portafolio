79836f81b033ad141a19e276e4d2249419067761
José Camacho <j.d.camacho27@gmail.com>
2021-07-29 16:46:15 -0400

    Cambios en las configuraciones de webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@templates': path.resolve(__dirname, 'src/templates'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+            '@images': path.resolve(__dirname, 'src/assets/images')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'stylus-loader'
+                ],
+            },
+            {
+                test: /\.png/,  // Tipos de fuentes a incluir
+                type: 'asset/resource',  // Tipo de módulo a usar (este mismo puede ser usado para archivos de imágenes)
+                generator: {
+                    filename: 'static/images/[name].[hash][ext][query]',  // Directorio de salida
+                },
+            },
+            {
+                test: /\.(woff|woff2)$/i,  // Tipos de fuentes a incluir
+                type: 'asset/resource',  // Tipo de módulo a usar (este mismo puede ser usado para archivos de imágenes)
+                generator: {
+                    filename: 'static/fonts/[name].[hash][ext][query]',  // Directorio de salida
+                },
+            },
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'static/[name].[contenthash].css'
+        }),
+        new Dotenv()
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    }
+    
+}
